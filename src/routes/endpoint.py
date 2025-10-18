359ba654b16d5c9f7fb2ba33bb504cdd1f1aa092
Asuriyu <cantarrannoelia@gmail.com>
2025-10-18 10:39:02 -0400

    eliminacion de carpetas

--- src/routes/endpoint.py
+++ src/routes/endpoint.py
@@ -6,9 +6,6 @@ class Endpoint(Enum):
 
     LOGIN = "/api/v2/admin/administrators/token"
 
-    BASE_TAXONS = "/api/v2/admin/taxons"
-    BASE_TAXONS_CODE = "/api/v2/admin/taxons/{code}"
-
     BASE_ADMINS = "/api/v2/admin/administrators"
     BASE_ADMINS_CODE = "/api/v2/admin/administrators/{id}"
 
